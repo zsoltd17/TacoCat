92aa8b6622a18419d903b8465c3cd1d049692499
Zsolt Dzsugán <zsoltd.business@gmail.com>
2023-02-06 22:47:29 +0100

    initial commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,22 @@
-// Call function
-function HelloWorld() {
-    alert("Hello World");
+const btnRun = document.querySelector('#btnSubmit');
+const result = document.querySelector('#result');
+
+function getValue() {
+    let inputStr = document.querySelector('#inputString').value;
+
+    inputStr = inputStr.toLowerCase().replace(/[^a-zA-Z ]/g, "");
+
+    let revStr = generateRevString(inputStr);
+
+    displayStr(inputStr, revStr);
 }
+
+function generateRevString(inputStr) {
+    return inputStr.split('').reverse().join('');
+}
+
+function displayStr(inputStr, revStr) {
+    result.innerHTML = (inputStr == revStr) ? `It's a palindrome! ${revStr}` : `Not a palindrome! ${revStr}`;
+}
+
+btnRun.addEventListener('click', getValue);
